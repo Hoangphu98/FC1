e5b9e98915804856c63b0b268e4387d1bc4d0788
Hoangphu98 <hoangphutk98@gmail.com>
2025-08-13 21:10:26 +0700

    BT1

---
+++ src/Lec2_variable/Exercise1.java
@@ -0,0 +1,17 @@
+package Lec2_variable;
+public class Exercise1 {
+    public static void main (String[] args) {
+        int x1=1+2;
+        double x2=3.5+8;
+        double x3= 1.0/2;
+        double x4=4.5+7;
+        int x6=20%3+2-7;
+        int x5=4*5+1;
+        System.out.println("1 + 2 = " + x1 );        
+        System.out.println("3.5 + 8 = " + x2 );      
+        System.out.println("1 / 2 = " + x3 );        
+        System.out.println("4.5 + 7 = " + x4 );       
+        System.out.println("4 * 5 + 1 = " + x5 );        
+        System.out.println("20 % 3 + 2 - 7 = " + x6 );
+    }
+}
