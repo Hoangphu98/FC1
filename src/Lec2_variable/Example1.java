8b74450f36172491a24a02cb1d0f008f39cd23d0
Hoangphu98 <hoangphutk98@gmail.com>
2025-08-13 11:06:06 +0700

    FirtCommit

---
+++ src/Lec2_variable/Example1.java
@@ -0,0 +1,23 @@
+package Lec2_variable;
+public class Example1 {
+    public static void main(String[] args) {
+        int x=1;
+        int x1=9/5;
+        int x2=695%20;
+        int x3=7+6*5;
+        int x4=7*6+5;
+        int x5=248%100/5;
+        int x6=6*3-9/4;
+        int x7=(5-7)*4;
+        int x8=6+(18%(17-12));
+        System.out.println("x = " + x);
+        System.out.println("9/5 = " + x1);
+        System.out.println("695%20 = " + x2);
+        System.out.println("7 + 6 * 5 = " + x3);
+        System.out.println("7 * 6 + 5 = " + x4);
+        System.out.println("248 % 100 / 5 = " + x5);
+        System.out.println("6 * 3 - 9 / 4 = " + x6);
+        System.out.println("(5 - 7) * 4 = " + x7);
+        System.out.println("6 + (18 % (17 - 12)) = " + x8);    
+    }
+}
