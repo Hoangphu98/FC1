fc6d4e195d4b82b6d4967cd89860b99c1fd6ad1a
Hoangphu98 <hoangphutk98@gmail.com>
2025-08-17 15:18:53 +0700

    BTScanner

--- src/Lec3_Scanner/Ex10.java
+++ src/Lec3_Scanner/Ex10.java
@@ -5,7 +5,7 @@ public class Ex10 {
     public static void main(String[] args) { 
         Scanner scanner = new Scanner(System.in); 
         scanner.useDelimiter(",|\\n"); 
-        System.out.println("Enter words separated by '##': "); 
+        System.out.println("Enter words separated by ',': "); 
         while (scanner.hasNext()) { 
             System.out.println(scanner.next()); 
         } 
